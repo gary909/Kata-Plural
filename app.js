ebb9fa0729e0cffcc4d0d98bdf37944f8c6dfac2
gary <gary.white@lodgit.com>
2022-05-02 13:15:12 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function plural(n) {
+    // ...
+}
+
+console.log(plural(0)); // true
+console.log(plural(0.5)); // true
+console.log(plural(1)); // false
+console.log(plural(100)); // true
+console.log(plural(Infinity)); // true
